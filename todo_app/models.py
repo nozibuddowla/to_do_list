437d2700565b65df370f06811f81083772e45dcc
nozibuddowla <nozibuddowla@gmail.com>
2024-12-11 20:52:36 +0600

    add due date in models

--- todo_app/models.py
+++ todo_app/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    due_date = models.DateField(null = True, blank = True) 
+
     def __str__(self):
         return self.title
